d7505a753b0baa481b501558b3e6ff2bae26f7f6
LvFJ2 <1462981484@qq.com>
2024-02-02 14:38:13 +0800

    第二次提交

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
-public class ScannerTest {
+public class Main {
     public static void main(String[] args){
         Scanner scanner=new Scanner(System.in);
         System.out.print("请输入一个数");
         int a=scanner.nextInt();
+        //System.out.printf("%d的平方是%d\n",a,a*a);
+
         System.out.printf("%d的平方是%d\n",a,a*a);
     }
 }
